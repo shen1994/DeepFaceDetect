d8ee30d53c54048b33636ca1dfdbbf0d40f3b439
shen1994 <843224306@qq.com>
2019-01-03 09:44:28 +0800

    revise test

--- test.py
+++ test.py
@@ -1,82 +1,69 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jun 24 20:58:33 2018
+Created on Wed Jul 25 09:36:44 2018
 
 @author: shen1994
 """
 
 import os
 import cv2
-import pickle
-from keras.preprocessing import image
-from scipy.misc import imread
-from scipy.misc import imresize
+import tensorflow as tf
 
-from ssd import SSD300
-from ssd_utils import BBoxUtility
-import matplotlib.pyplot as plt
-import numpy as np
+from tssd_utils import BBoxUtility
 
 if __name__ == "__main__":
     
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    
-    inputs = []
-    images = []
-    
-    image_path = 'images/test/0--Parade/0_Parade_marchingband_1_32.jpg' # 'timg.jpg'# 'images/test/0--Parade/0_Parade_marchingband_1_32.jpg'
-    img = image.load_img(image_path, target_size=(720, 720))
-    # img = cv2.imread(image_path, 1)
-    img = image.img_to_array(img)
-    images.append(imread(image_path))
-    inputs.append(img.copy())
-    inputs = np.array(inputs)
-    
-    model = SSD300([720, 720, 3], num_classes=2)
-    model.load_weights('model/weights.30-1.81.hdf5', by_name=True)
-    
-    preds = model.predict(inputs, batch_size=1, verbose=0)
-    priors = pickle.load(open('model/prior_boxes.pkl', 'rb'))
-    bbox_util = BBoxUtility(2, priors, is_decoder=True)
-    results = bbox_util.detection_out(preds)
-    
-    for i, img in enumerate(images):
-    # Parse the outputs.
-        det_label = results[i][:, 0]
-        det_conf = results[i][:, 1]
-        det_xmin = results[i][:, 2]
-        det_ymin = results[i][:, 3]
-        det_xmax = results[i][:, 4]
-        det_ymax = results[i][:, 5]
 
-        # Get detections with confidence higher than 0.6.
-        top_indices = [i for i, conf in enumerate(det_conf) if conf >= 0.68]
+    # face detect model
+    detect_graph_def = tf.GraphDef()
+    detect_graph_def.ParseFromString(open("model/pico_FaceDetect_model.pb", "rb").read())
+    detect_tensors = tf.import_graph_def(detect_graph_def, name="")
+    detect_sess = tf.Session()
+    detect_opt = detect_sess.graph.get_operations()
+    detect_x = detect_sess.graph.get_tensor_by_name("input_1:0")
+    detect_y = detect_sess.graph.get_tensor_by_name("predictions/concat:0")
+    detect_util = BBoxUtility(detect_sess, 2, top_k=8)
 
+    cv2.namedWindow("DeepFace", cv2.WINDOW_NORMAL)
+    
+    while(True):
+
+        # read one image
+        o_image = cv2.imread("test.jpg", 1)
+        width, height, channel = o_image.shape
+    
+        detect_image = cv2.resize(o_image, (720, 720))
+        detect_out = detect_sess.run(detect_y, feed_dict={detect_x: [detect_image]})
+        results = detect_util.detection_out(detect_out) 
+        det_conf = results[0][:, 1]
+        det_xmin = results[0][:, 2]
+        det_ymin = results[0][:, 3]
+        det_xmax = results[0][:, 4]
+        det_ymax = results[0][:, 5]
+        top_indices = [i for i, conf in enumerate(det_conf) if conf >= 0.57]
         top_conf = det_conf[top_indices]
-        top_label_indices = det_label[top_indices].tolist()
         top_xmin = det_xmin[top_indices]
         top_ymin = det_ymin[top_indices]
         top_xmax = det_xmax[top_indices]
         top_ymax = det_ymax[top_indices]
-
-        colors = plt.cm.hsv(np.linspace(0, 1, 4)).tolist()
-
-        plt.imshow(img / 255.)
-        currentAxis = plt.gca()
-
+    
         for i in range(top_conf.shape[0]):
-            xmin = int(round(top_xmin[i] * img.shape[1]))
-            ymin = int(round(top_ymin[i] * img.shape[0]))
-            xmax = int(round(top_xmax[i] * img.shape[1]))
-            ymax = int(round(top_ymax[i] * img.shape[0]))
-            score = top_conf[i]
-            label = int(top_label_indices[i])
-            # label_name = voc_classes[label - 1]
-            display_txt = '{:0.2f}, {}'.format(score, label)
-            coords = (xmin, ymin), xmax-xmin+1, ymax-ymin+1
-            color = colors[label]
-            currentAxis.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
-            currentAxis.text(xmin, ymin, display_txt, bbox={'facecolor':color, 'alpha':0.5})
-   
-        plt.show()
-        
+            
+            # box
+            xmin = int(round(top_xmin[i] * height))
+            ymin = int(round(top_ymin[i] * width))
+            xmax = int(round(top_xmax[i] * height))
+            ymax = int(round(top_ymax[i] * width))
+          
+            # draw some lines, points, text
+            cv2.rectangle(o_image, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
+
+        cv2.imshow("DeepFace", o_image)
+            
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):
+            break
+    
+    # release all resources
+    cv2.destroyAllWindows()
+    detect_sess.close() 
---
+++ tssd_utils.py
@@ -0,0 +1,112 @@
+"""Some utils for SSD."""
+
+import numpy as np
+import tensorflow as tf
+
+
+class BBoxUtility(object):
+    """Utility class to do some stuff with bounding boxes and priors.
+
+    # Arguments
+        num_classes: Number of classes including background.
+        priors: Priors and variances, numpy tensor of shape (num_priors, 8),
+            priors[i] = [xmin, ymin, xmax, ymax, varxc, varyc, varw, varh].
+        overlap_threshold: Threshold to assign box to a prior.
+        nms_thresh: Nms threshold.
+        top_k: Number of total bboxes to be kept per image after nms step.
+
+    # References
+        https://arxiv.org/abs/1512.02325
+    """
+    # TODO add setter methods for nms_thresh and top_K
+    def __init__(self, session, num_classes, nms_thresh=0.32, top_k=15):
+        self.num_classes = num_classes
+        self._nms_thresh = nms_thresh
+        self._top_k = top_k
+        self.boxes = tf.placeholder(dtype='float32', shape=(None, 4))
+        self.scores = tf.placeholder(dtype='float32', shape=(None,))
+        self.nms = tf.image.non_max_suppression(self.boxes, self.scores,
+                                                self._top_k,
+                                                iou_threshold=self._nms_thresh)
+        self.sess = session
+
+    def decode_boxes(self, mbox_loc, mbox_priorbox, variances):
+        """Convert bboxes from local predictions to shifted priors.
+
+        # Arguments
+            mbox_loc: Numpy array of predicted locations.
+            mbox_priorbox: Numpy array of prior boxes.
+            variances: Numpy array of variances.
+
+        # Return
+            decode_bbox: Shifted priors.
+        """
+        prior_width = mbox_priorbox[:, 2] - mbox_priorbox[:, 0]
+        prior_height = mbox_priorbox[:, 3] - mbox_priorbox[:, 1]
+        prior_center_x = 0.5 * (mbox_priorbox[:, 2] + mbox_priorbox[:, 0])
+        prior_center_y = 0.5 * (mbox_priorbox[:, 3] + mbox_priorbox[:, 1])
+        decode_bbox_center_x = mbox_loc[:, 0] * prior_width * variances[:, 0]
+        decode_bbox_center_x += prior_center_x
+        decode_bbox_center_y = mbox_loc[:, 1] * prior_width * variances[:, 1]
+        decode_bbox_center_y += prior_center_y
+        decode_bbox_width = np.exp(mbox_loc[:, 2] * variances[:, 2])
+        decode_bbox_width *= prior_width
+        decode_bbox_height = np.exp(mbox_loc[:, 3] * variances[:, 3])
+        decode_bbox_height *= prior_height
+        decode_bbox_xmin = decode_bbox_center_x - 0.5 * decode_bbox_width
+        decode_bbox_ymin = decode_bbox_center_y - 0.5 * decode_bbox_height
+        decode_bbox_xmax = decode_bbox_center_x + 0.5 * decode_bbox_width
+        decode_bbox_ymax = decode_bbox_center_y + 0.5 * decode_bbox_height
+        decode_bbox = np.concatenate((decode_bbox_xmin[:, None],
+                                      decode_bbox_ymin[:, None],
+                                      decode_bbox_xmax[:, None],
+                                      decode_bbox_ymax[:, None]), axis=-1)
+        decode_bbox = np.minimum(np.maximum(decode_bbox, 0.0), 1.0)
+        return decode_bbox
+
+    def detection_out(self, predictions, confidence_threshold=0.01):
+        """Do non maximum suppression (nms) on prediction results.
+
+        # Arguments
+            predictions: Numpy array of predicted values.
+            num_classes: Number of classes for prediction.
+            background_label_id: Label of background class.
+            keep_top_k: Number of total bboxes to be kept per image
+                after nms step.
+            confidence_threshold: Only consider detections,
+                whose confidences are larger than a threshold.
+
+        # Return
+            results: List of predictions for every picture. Each prediction is:
+                [label, confidence, xmin, ymin, xmax, ymax]
+        """
+        mbox_loc = predictions[:, :, :4]
+        variances = predictions[:, :, -4:]
+        mbox_priorbox = predictions[:, :, -8:-4]
+        mbox_conf = predictions[:, :, 4:-8]
+        results = []
+        for i in range(len(mbox_loc)):
+            results.append([])
+            decode_bbox = self.decode_boxes(mbox_loc[i],
+                                            mbox_priorbox[i], variances[i])
+            c_confs = mbox_conf[i, :, 1]
+            c_confs_m = c_confs > 0.01
+            if len(c_confs[c_confs_m]) > 0:
+                boxes_to_process = decode_bbox[c_confs_m]
+                confs_to_process = c_confs[c_confs_m]
+                feed_dict = {self.boxes: boxes_to_process,
+                             self.scores: confs_to_process}
+                idx = self.sess.run(self.nms, feed_dict=feed_dict)
+                good_boxes = boxes_to_process[idx]
+                confs = confs_to_process[idx][:, None]
+                labels = 1 * np.ones((len(idx), 1))
+                c_pred = np.concatenate((labels, confs, good_boxes),
+                         axis=1)
+                results[-1].extend(c_pred)
+
+            if len(results[-1]) > 0:
+                results[-1] = np.array(results[-1])
+                argsort = np.argsort(results[-1][:, 1])[::-1]
+                results[-1] = results[-1][argsort]
+                results[-1] = results[-1][:self._top_k]
+        return results
